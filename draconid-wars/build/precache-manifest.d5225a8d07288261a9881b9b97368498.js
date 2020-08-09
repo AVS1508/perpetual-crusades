517bdcfa7078bac3fd402552fd78ec32a64281f0
AVS1508 <aditya.11.present@gmail.com>
2020-08-09 20:52:12 +0530

    GitHub Page Deployment

---
+++ draconid-wars/build/precache-manifest.d5225a8d07288261a9881b9b97368498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb069b9c43c471d7f07db72cf98c6fa",
+    "url": "/AVS1508/draconid-wars/index.html"
+  },
+  {
+    "revision": "0d13a2ec6051db6a760f",
+    "url": "/AVS1508/draconid-wars/static/css/main.7908cf07.chunk.css"
+  },
+  {
+    "revision": "10e8551c6ce94b1eb818",
+    "url": "/AVS1508/draconid-wars/static/js/2.2e70a77b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AVS1508/draconid-wars/static/js/2.2e70a77b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d13a2ec6051db6a760f",
+    "url": "/AVS1508/draconid-wars/static/js/main.62fa832a.chunk.js"
+  },
+  {
+    "revision": "c13ad6615c5756b65100",
+    "url": "/AVS1508/draconid-wars/static/js/runtime-main.b8d5d157.js"
+  }
+]);
